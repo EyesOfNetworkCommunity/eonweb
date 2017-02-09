9b41e76a1f57e7c98d2a838b02b885b6e8f4649d
Jean-Philippe Levy <jeanphilippe.levy@gmail.com>
2017-02-09 11:32:52 +0100

    Add version in about

--- module/help_about/index.php
+++ module/help_about/index.php
@@ -29,7 +29,7 @@ include("../../side.php");
 
 	<div class="row">
 		<div class="col-lg-12">
-			<h1 class="page-header"><?php echo getLabel("label.home_about.title"); ?></h1>
+			<h1 class="page-header"><?php echo getLabel("label.home_about.title")." $version"; ?></h1>
 		</div>
 	</div>
 
