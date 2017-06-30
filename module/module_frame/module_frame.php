916747c4bbaae711bbbb7f7bcc3f4dff829ca667
Jean-Philippe Levy <jeanphilippe.levy@gmail.com>
2017-06-30 14:17:56 +0200

    Update bower components and fix module_frame

--- module/module_frame/module_frame.php
+++ module/module_frame/module_frame.php
@@ -27,39 +27,48 @@ $url=retrieve_form_data("url",null);
 <script>
 if (window !=top ) {top.location=window.location;}
 
-$(function() {
-	$(window).load(function() {
-		$('#page-wrapper').empty();
-        	$('<iframe>', {
-        	        src: '<?php echo urldecode($url); ?>',
-        	        id:  'iframe',
-        	        class: 'iframe',
-        	        frameborder: 0,
-        	}).appendTo('#page-wrapper');
-	});
-	$(window).bind("load resize", function() {
-		topOffset = $(".navbar-static-top").height() + 1;
-		leftOffset = $(".navbar-default.sidebar").width();
-		width = (this.window.innerWidth > 0) ? this.window.innerWidth : this.screen.width;
-		if (width <= 768) {
-			height = $("body")[0].scrollHeight;
-			leftOffset=0
-		} else { 
-			height = ((this.window.innerHeight > 0) ? this.window.innerHeight : this.screen.height) - 1;
-		}
-		height = height - topOffset;
-		if (height < 1) height = 1;
-		$(".iframe").css("top",topOffset);
-		$(".iframe").css("left",leftOffset);
-		$(".iframe").height(height-$(".footer").height());
-		$(".iframe").width(width-leftOffset);
-		$(".iframe").show();
-	});
-	$("#menu-toggle").on("click", function() {
-		leftOffset = $(".navbar-default.sidebar").width() + 5;
-		width = (window.innerWidth > 0) ? window.innerWidth : screen.width;
-		$(".iframe").css("left",leftOffset);
-		$(".iframe").width(width-leftOffset);
-	});
+var navbarwidth = 0;
+$(document).ready(function(){
+	navbarwidth = $(".navbar-default.sidebar").width();
 });
+
+$(window).on('load',function() {
+	$('#page-wrapper').empty();
+	$('<iframe>', {
+		src: '<?php echo urldecode($url); ?>',
+		id:  'iframe',
+		class: 'iframe',
+		frameborder: 0,
+	}).appendTo('#page-wrapper');
+});
+
+$(window).bind("load resize", function() {
+	topOffset = $(".navbar-static-top").height() + 1;
+	leftOffset = $(".navbar-default.sidebar").width();
+	width = (this.window.innerWidth > 0) ? this.window.innerWidth : this.screen.width;
+	if (width <= 768) {
+		height = $("body")[0].scrollHeight;
+		leftOffset=0
+	} else { 
+		height = ((this.window.innerHeight > 0) ? this.window.innerHeight : this.screen.height) - 1;
+	}
+	height = height - topOffset;
+	if (height < 1) height = 1;
+	$(".iframe").css("top",topOffset);
+	$(".iframe").css("left",leftOffset);
+	$(".iframe").height(height-$(".footer").height());
+	$(".iframe").width(width-leftOffset);
+	$(".iframe").show();
+});
+
+$("#menu-toggle").on("click", function() {
+	leftOffset = 5;
+	if($(".navbar-default.sidebar").width()!=navbarwidth) {
+		leftOffset = leftOffset + navbarwidth;
+	}
+	width = (window.innerWidth > 0) ? window.innerWidth : screen.width;
+	$(".iframe").css("left",leftOffset);
+	$(".iframe").width(width-leftOffset);
+});
+
 </script>
