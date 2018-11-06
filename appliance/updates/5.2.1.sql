6804d0ac053bc0d3e5e732e1eb72b7bba9bfd326
Rémi VERCHERE <remi.verchere@axians.com>
2018-11-06 16:56:33 +0100

    Add notifier tracking column after timee_period to leave sort_key at the end of table

--- appliance/updates/5.2.1.sql
+++ appliance/updates/5.2.1.sql
@@ -1,2 +1,3 @@
 USE notifier;
-ALTER TABLE rules ADD COLUMN tracking INT(11) NULL DEFAULT '0';
+ALTER TABLE rules ADD COLUMN tracking TINYINT(1) NOT NULL DEFAULT 0 AFTER timeperiod_id;
+UPDATE rules SET tracking=0;
