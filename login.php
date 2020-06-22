6341c5804cdc1c8e046c9ebea851b478419bcd17
Hoarau Jeremy <jeremy.hoarau@axians.com>
2020-06-22 10:46:26 +0200

    Remove  displayed errors

--- login.php
+++ login.php
@@ -19,9 +19,9 @@
 #
 #########################################
 */
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+// ini_set('display_errors', 1);
+// ini_set('display_startup_errors', 1);
+// error_reporting(E_ALL);
 include("header.php");
 
 // Display login Form
