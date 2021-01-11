c5d197e221b3e7d47fadadbea2e0f46949857371
Oscar Poels <o.poels@gmail.com>
2021-01-11 14:05:42 +0100

    complexify session_id to prevent brut force #82

--- login.php
+++ login.php
@@ -259,7 +259,7 @@ else {
 			$usrlimit = $usersql[0]["user_limitation"];
 
 			// Create session ID
-			$sessid=rand();
+			$sessid=$key = substr(strtolower(md5(microtime().rand(1000, 9999))), 0, 25);
 			sql($database_eonweb,"INSERT INTO sessions (session_id,user_id) VALUES (?,?)", array($sessid, $usrid));
 			
 			// Send cookie
