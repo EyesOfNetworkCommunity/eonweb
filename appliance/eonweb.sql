112ab72d2559bfd5c0e8799b6e1188fcf51e6890
Julien1498 <53144809+Julien1498@users.noreply.github.com>
2021-12-20 13:55:06 +0100

    Update sql appliance

--- appliance/eonweb.sql
+++ appliance/eonweb.sql
@@ -299,6 +299,54 @@ UNLOCK TABLES;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
+--
+-- Table structure for table `reports`
+--
+
+DROP TABLE IF EXISTS `reports`;
+CREATE TABLE reports
+(
+    id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    reportName VARCHAR(100) UNIQUE,
+    period VARCHAR(20),
+    cron VARCHAR(20)
+);
+
+--
+-- Table structure for table `reports_hosts`
+--
+
+DROP TABLE IF EXISTS `reports_hosts`;
+CREATE TABLE reports_hosts
+(
+    id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    hostName VARCHAR(100),
+    reportsID INT
+);
+
+--
+-- Table structure for table `reports_services`
+--
+
+DROP TABLE IF EXISTS `reports_services`;
+CREATE TABLE reports_services
+(
+    id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    serviceName VARCHAR(100),
+    reportsID INT
+);
+
+--
+-- Table structure for table `reports_emails`
+--
+
+DROP TABLE IF EXISTS `reports_emails`;
+CREATE TABLE reports_emails
+(
+    id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    email VARCHAR(100),
+    reportsID INT
+);
 
 --
 -- Table structure for table `itsm_champ_ged`
